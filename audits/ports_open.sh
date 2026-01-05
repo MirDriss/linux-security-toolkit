e620f6582c84eba7876742b40ce9390a59ef3992
Mir Driss <driss94600mun@gmail.com>
2026-01-05 21:09:45 +0100

    Update of the display of audit scripts

--- audits/ports_open.sh
+++ audits/ports_open.sh
@@ -1,7 +1,22 @@
 #!/bin/bash 
 
+RESET="\033[0m"
+
 total=$(ss -tulnp | tail -n +2 | awk '{print $5}' | awk -F: '{print $NF}' | sort | uniq | wc -l)
-echo "Total ports open detected: $total"
+function status_color(){
+	if [ "$total" -lt 5 ]; then 
+		STATUS="OK"
+		COLOR="\033[32m"
+	elif [ "$total" -lt 10 ]; then 
+		STATUS="WARNING"
+		COLOR="\033[33m"
+	else
+		STATUS="CRITICAL"
+		COLOR="\033[31m"
+	fi
+}
+status_color
+echo -e "${COLOR}[$STATUS]${RESET} Total ports open detected: $total"
 while read port; do 
 	echo "Port open detected: $port"
 	 ((count++))
--- audits/services_running.sh
+++ audits/services_running.sh
@@ -1,8 +1,22 @@
 #!/bin/bash 
 
+RESET="\033[0m"
 total=$(systemctl list-units --type=service --state=running | grep ".service" | wc -l)
 
-echo "Total running services detected: $total"
+function status_color(){
+        if [ "$total" -lt 30 ]; then 
+                STATUS="OK"
+                COLOR="\033[32m"
+        elif [ "$total" -lt 60 ]; then 
+                STATUS="WARNING"
+                COLOR="\033[33m"
+        else
+                STATUS="CRITICAL"
+                COLOR="\033[31m"
+        fi
+}
+status_color
+echo -e "${COLOR}[$STATUS]${RESET} Total running services detected: $total"
 
 while read line; do 
 	service=$(echo "$line" | awk '{print $1}')
