b340c4522479fd31f08b3beecbfc306916f96d81
Mir Driss <driss94600mun@gmail.com>
2026-01-06 10:55:40 +0100

    Centralization of functions in the tools.sh script

--- audits/tools.sh
+++ audits/tools.sh
@@ -4,7 +4,7 @@ RESET="\033[0m"
 RED="\033[31m"
 YELLOW="\033[33m"
 GREEN="\033[32m"
-
+BLUE="\033[34m"
 function get_status_color(){
 	local sum="$1"
 	local warn="$2"
@@ -17,3 +17,19 @@ function get_status_color(){
 		echo -e "${RED}[CRITICAL]${RESET}"
 	fi
 } 
+
+function log_level(){
+        level="$1"
+        message="$2"
+        case "$level" in 
+                INFO) color="$BLUE" ;;
+                OK) color="$GREEN" ;;
+                WARNING) color="$YELLOW" ;;
+                ALERT|CRITICAL) color="$RED" ;;
+                *) color="$RESET" ;;
+        esac 
+        # Write in the terminal 
+        echo -e "${color}[$level] ${RESET}$message " 
+        # Write in the file 
+        echo "[$level] $message" >> "$REPORT_FILE"
+}
--- security_audit.sh
+++ security_audit.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-RESET="\033[0m"
 DIR_SCRIPT="$(cd "$(dirname "$0")" && pwd)"
-
+source "$DIR_SCRIPT"/audits/tools.sh
 DIRECTORY="reports"
 
 if [ ! -d "$DIRECTORY" ]; then
@@ -14,23 +9,6 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 
 REPORT_FILE="$DIRECTORY/audit_$(date -I).txt"
-
-function log_level(){
-        level="$1"
-        message="$2"
-        case "$level" in 
-                INFO) color="$BLUE" ;;
-                OK) color="$GREEN" ;;
-                WARNING) color="$YELLOW" ;;
-                ALERT|CRITICAL) color="$RED" ;;
-                *) color="$RESET" ;;
-        esac 
-        # Write in the terminal 
-        echo -e "${color}[$level] ${RESET}$message " 
-        # Write in the file 
-        echo "[$level] $message" >> "$REPORT_FILE"
-}
-
 log_level INFO "===== LINUX SECURITY AUDIT ====="
 log_level INFO "Audit executed at $(date '+%Y-%m-%d %H:%M:%S') by $(hostname)" 
 log_level INFO "------------------------"
